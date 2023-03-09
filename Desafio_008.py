6c47b79f906323dad90220becaa0e8d454e61119
Fernando Augusto <91295365+fernandoaugusto0998@users.noreply.github.com>
2023-03-09 16:20:14 -0400

    Delete Desafio_008.py

--- Desafio_008.py
+++
@@ -1,4 +0,0 @@
-salario = float(input('Digite o valor do salário do funcionário: '))
-aumento = salario * 0.15
-salarioaumentado = salario + aumento
-print('O seu salário com aumento é R${}, você teve um aumento de R${}.' .format(salarioaumentado, aumento))
