b6ed8ec8cc54cc6b18cfc02c3a0a1a7a9f7be6da
Fernando Augusto <91295365+fernandoaugusto0998@users.noreply.github.com>
2023-03-09 16:19:08 -0400

    Delete Desafio_003.py

--- Desafio_003.py
+++
@@ -1,6 +0,0 @@
-#Escreva um programa que leia um valor em metros e o exiba convertido em centímetros e milímetros.
-
-m = float(input('Informe o valor em metros:'))
-mm = m * 1000
-
-print('%.f A quantidade de metros equivalem á %.f milímetros:' %(m, mm))
