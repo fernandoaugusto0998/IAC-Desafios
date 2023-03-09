58844ebf336bfd3ab4847ea3eeb24930a41fe385
Fernando Augusto <91295365+fernandoaugusto0998@users.noreply.github.com>
2023-03-09 16:19:01 -0400

    Delete Desafio_002.py

--- Desafio_002.py
+++
@@ -1,8 +0,0 @@
-#Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média.
-
-n1 = float(input('Informe a primeira nota:'))
-n2 = float(input('Informe a segunda nota:'))
-
-media = (n1 + n2) / 2
-
-print('A média é:' ,media)
